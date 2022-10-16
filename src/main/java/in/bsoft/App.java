ebac8fde40857c3273d9a189bb3dd828669dfa9a
ParashaktiSriram <bharath.kon@gmail.com>
2022-10-16 12:16:06 +0530

    first commit

---
+++ src/main/java/in/bsoft/App.java
@@ -0,0 +1,13 @@
+package in.bsoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
